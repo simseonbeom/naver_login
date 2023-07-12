a9035eaa50da7968e98ca508c79e29c59dd7ae12
kindtiger <seonbeom2@gmail.com>
2023-07-12 16:28:17 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+const user = {
+  id:'asd@naver.com',
+  pw:'spdlqj123!@'
+}
+
+/*
+
+1. email 정규표현식을 사용한 validation
+2. pw 정규표현식을 사용한 validation
+3. 상태 변수 관리
+4. 로그인 버튼을 클릭시 조건처리
+
+*/
+
+function emailReg(text){
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(String(text).toLowerCase())
+}
+
+function pwReg(text){
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+  return re.test(String(text).toLowerCase());
+}
+
+
+
+
+
+
+
+
+
+
+
